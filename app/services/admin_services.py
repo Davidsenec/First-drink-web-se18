9f3c7336c4501269ff8343475b9582ea4e8037a6
auto <autokurtz18@gmail.com>
2026-07-20 22:20:57 +0700

    add admin can delete user

--- app/services/admin_services.py
+++ app/services/admin_services.py
@@ -28,3 +28,17 @@ class AdminServices:
                 detail="Access denied.",
             )
         return self.repo.get_all_data()
+
+    def delete(self, id: int, current_admin: User):
+        user = self.repo.check_id(id)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Not Found",
+            )
+        if not current_admin.is_admin or user.is_admin:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Access denied.",
+            )
+        return self.repo.delete_user(user)
